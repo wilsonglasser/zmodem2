b25109dced9a1958f7f11e7c66137f3d21a97918
Jarkko Sakkinen <jarkko.sakkinen@iki.fi>
2025-09-08 16:19:46 +0300

    Revert "tests: remove integration tests"
    This reverts commit ef9bcb0e8ef047bebf676f6aa2b539da4f15f96b.

--- build.rs
+++ build.rs
@@ -4,6 +4,7 @@ use std::process::Command;
 
 fn main() {
     println!("cargo:rerun-if-env-changed=OUT_DIR");
+    println!("cargo:rustc-check-cfg=cfg(host_has_rzsz)");
 
     // rzsz
     if Command::new("rz").spawn().is_ok() && Command::new("sz").spawn().is_ok() {
